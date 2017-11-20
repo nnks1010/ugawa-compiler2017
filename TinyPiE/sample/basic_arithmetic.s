c7f5fd2be1ee85ef60ca8bf76ac6313a73efb59f
nnks <ntomoya1010@gmail.com>
2017-11-20 23:41:53 +0900

    generator assembly

--- TinyPiE/sample/basic_arithmetic.s
+++ TinyPiE/sample/basic_arithmetic.s
@@ -10,18 +10,24 @@ _Pi_var_z:
 	.global _start
 _start:
 	@ 式をコンパイルした命令列
-	ldr r0, =#2
-	str r1, [sp, #-4]!
-	mov r1, r0
 	ldr r0, =_Pi_var_x
 	ldr r0, [r0, #0]
 	str r1, [sp, #-4]!
 	mov r1, r0
 	ldr r0, =_Pi_var_y
 	ldr r0, [r0, #0]
-	add r0, r1, r0
-	ldr r1, [sp], #4
 	mul r0, r1, r0
+	ldr r1, [sp], #4
+	str r1, [sp, #-4]!
+	mov r1, r0
+	ldr r0, =#5
+	str r1, [sp, #-4]!
+	mov r1, r0
+	ldr r0, =_Pi_var_z
+	ldr r0, [r0, #0]
+	mul r0, r1, r0
+	ldr r1, [sp], #4
+	sub r0, r1, r0
 	ldr r1, [sp], #4
 	@ EXIT システムコール
 	mov r7, #1
