d95d22ab785fb639376943271efa1ea2ff8783ba
nnks <ntomoya1010@gmail.com>
2017-12-02 02:36:49 +0900

    exercises 6

--- TinyPiE/src/Interpreter.java
+++ TinyPiE/src/Interpreter.java
@@ -15,12 +15,21 @@ public class Interpreter extends InterpreterBase {
 			int rhsValue = evalExpr(nd.rhs, env);
 			if (nd.op.equals("+"))
 				return lhsValue + rhsValue;
-			if (nd.op.equals("-"))
+			else if (nd.op.equals("-"))
 				return lhsValue - rhsValue;
 			else if (nd.op.equals("*"))
 				return lhsValue * rhsValue;
 			else if (nd.op.equals("/"))
 				return lhsValue / rhsValue;
+			else
+				throw new Error("Unknown operator: " + nd.op);
+		} else if (ndx instanceof ASTUnaryExprNode) {
+			ASTUnaryExprNode nd = (ASTUnaryExprNode) ndx;
+			int operand = evalExpr(nd.operand, env);
+			if (nd.op.equals("-"))
+				return -1 * operand;
+			else if (nd.op.equals("^"))
+				return ~operand;
 			else
 				throw new Error("Unknown operator: " + nd.op);
 		} else if (ndx instanceof ASTNumberNode) {
