9d00281f26d37255ffa3fe58046c9b46c7201f57
nnks <ntomoya1010@gmail.com>
2017-12-02 15:37:59 +0900

    exercises 9

--- TinyPiS/src/Compiler.java
+++ TinyPiS/src/Compiler.java
@@ -8,6 +8,49 @@ import parser.TinyPiSLexer;
 import parser.TinyPiSParser;
 
 public class Compiler extends CompilerBase {
+	void compileStmt(ASTNode ndx, Environment env) {
+		if (ndx instanceof ASTCompoundStmtNode) {
+			ASTCompoundStmtNode nd = (ASTCompoundStmtNode) ndx;
+			for (ASTNode child: nd.stmts)
+				compileStmt(child, env);
+		} else if (ndx instanceof ASTAssignStmtNode) {
+			ASTAssignStmtNode nd = (ASTAssignStmtNode) ndx;
+			Variable var = env.lookup(nd.var);
+			if (var == null)
+				throw new Error("Undefined variable: "+nd.var);
+			compileExpr(nd.expr, env);
+		    if (var instanceof GlobalVariable) {
+		    	GlobalVariable globalVar = (GlobalVariable) var;
+		    	emitLDC(REG_R1, globalVar.getLabel());
+		    	emitSTR(REG_DST, REG_R1, 0);
+		    } else
+		    	throw new Error("Not a global variable: "+nd.var);
+		} else if (ndx instanceof ASTIfStmtNode) {
+			ASTIfStmtNode nd = (ASTIfStmtNode) ndx;
+			String elseLabel = freshLabel();
+			String endLabel = freshLabel();
+			compileExpr(nd.cond, env);
+			emitRI("cmp", REG_DST, 0);
+			emitJMP("beq", elseLabel);
+			compileStmt(nd.thenClause, env);
+			emitJMP("b", endLabel);
+			emitLabel(elseLabel);
+			compileStmt(nd.elseClause, env);
+			emitLabel(endLabel);
+		} else if (ndx instanceof ASTWhileStmtNode) {
+			ASTWhileStmtNode nd = (ASTWhileStmtNode) ndx;
+			String loopLabel = freshLabel();
+			String endLabel = freshLabel();
+			emitLabel(loopLabel);
+			compileExpr(nd.cond, env);
+			emitRI("cmp", REG_DST, 0);
+			emitJMP("bne", endLabel);
+			compileStmt(nd.stmt, env);
+			emitJMP("b", loopLabel);
+			emitLabel(endLabel);
+		} else
+			throw new Error("Unknown expression: "+ndx);
+	}
 	void compileExpr(ASTNode ndx, Environment env) {
 		if (ndx instanceof ASTBinaryExprNode) {
 			ASTBinaryExprNode nd = (ASTBinaryExprNode) ndx;
@@ -46,25 +89,33 @@ public class Compiler extends CompilerBase {
 	
 	void compile(ASTNode ast) {
 		Environment env = new Environment();
-		GlobalVariable vx = addGlobalVariable(env, "x");
-		GlobalVariable vy = addGlobalVariable(env, "y");
-		GlobalVariable vz = addGlobalVariable(env, "z");
+		ASTProgNode prog = (ASTProgNode) ast;
 
 		System.out.println("\t.section .data");
 		System.out.println("\t@ 大域変数の定義");
-		emitLabel(vx.getLabel());
-		System.out.println("\t.word 1");
-		emitLabel(vy.getLabel());
-		System.out.println("\t.word 10");
-		emitLabel(vz.getLabel());
-		System.out.println("\t.word -1");
+		for (String varName: prog.varDecls) {
+			if (env.lookup(varName) != null)
+				throw new Error("Variable redefined: "+varName);
+			GlobalVariable v = addGlobalVariable(env, varName);
+			emitLabel(v.getLabel());
+			System.out.println("\t.word 0");
+		}
+		if (env.lookup("answer") == null) {
+			GlobalVariable v = addGlobalVariable(env, "answer");
+			emitLabel(v.getLabel());
+			System.out.println("\t.word 0");
+		}
+
 		System.out.println("\t.section .text");
 		System.out.println("\t.global _start");
 		System.out.println("_start:");
 		System.out.println("\t@ 式をコンパイルした命令列");
-		compileExpr(ast, env);
+		compileStmt(prog.stmt, env);
 		System.out.println("\t@ EXITシステムコール");
-		emitRI("mov", "r7", 1);   // EXIT のシステムコール番号
+		GlobalVariable v = (GlobalVariable) env.lookup("answer");
+		emitLDC(REG_DST, v.getLabel());	// 変数 answer の値を ro (終了コード)に入れる
+		emitLDR("r0", REG_DST, 0);
+		emitRI("mov", "r7", 1);			// EXIT のシステムコール番号
 		emitI("swi", 0);
 	}
 
--- TinyPiS/src/Interpreter.java
+++ TinyPiS/src/Interpreter.java
@@ -8,6 +8,35 @@ import parser.TinyPiSLexer;
 import parser.TinyPiSParser;
 
 public class Interpreter extends InterpreterBase {
+	void evalStmt(ASTNode ndx, Environment env) {
+		if (ndx instanceof ASTCompoundStmtNode) {
+			ASTCompoundStmtNode nd = (ASTCompoundStmtNode) ndx;
+			for (ASTNode child: nd.stmts)
+				evalStmt(child, env);
+		} else if (ndx instanceof ASTAssignStmtNode) {
+			ASTAssignStmtNode nd = (ASTAssignStmtNode) ndx;
+			Variable var = env.lookup(nd.var);
+			if (var == null)
+				throw new Error("Undefined variable: "+nd.var);
+			int value = evalExpr(nd.expr, env);
+			var.set(value);
+		} else if (ndx instanceof ASTIfStmtNode) {
+			ASTIfStmtNode nd = (ASTIfStmtNode) ndx;
+			if (evalExpr(nd.cond, env) != 0)
+				evalStmt(nd.thenClause, env);
+			else
+				evalStmt(nd.elseClause, env);
+		} else if (ndx instanceof ASTWhileStmtNode) {
+			ASTWhileStmtNode nd = (ASTWhileStmtNode) ndx;
+			if (evalExpr(nd.cond, env) != 0) {
+				evalStmt(nd.stmt, env);
+				evalStmt(nd, env);
+			}
+		} else {
+			throw new Error("Unknown statement: "+ndx);
+		}
+	}
+
 	int evalExpr(ASTNode ndx, Environment env) {
 		if (ndx instanceof ASTBinaryExprNode) {
 			ASTBinaryExprNode nd = (ASTBinaryExprNode) ndx;
@@ -39,10 +68,17 @@ public class Interpreter extends InterpreterBase {
 
 	public int eval(ASTNode ast) {
 		Environment env = new Environment();
-		addGlobalVariable(env, "x", 1);
-		addGlobalVariable(env, "y", 10);
-		addGlobalVariable(env, "z", -1);		
-		return evalExpr(ast, env);
+		ASTProgNode prog = (ASTProgNode) ast;
+		for (String varName: prog.varDecls) {
+			if (env.lookup(varName) != null)
+				throw new Error("Variable redefined: "+varName);
+			addGlobalVariable(env, varName, 0);
+		}
+		if (env.lookup("answer") == null)
+			addGlobalVariable(env, "answer", 0);
+		evalStmt(prog.stmt, env);
+		Variable varAnswer = env.lookup("answer");
+		return varAnswer.get();
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -50,7 +86,7 @@ public class Interpreter extends InterpreterBase {
 		TinyPiSLexer lexer = new TinyPiSLexer(input);
 		CommonTokenStream token = new CommonTokenStream(lexer);
         TinyPiSParser parser = new TinyPiSParser(token);
-        ParseTree tree = parser.expr();
+        ParseTree tree = parser.prog();
         ASTGenerator astgen = new ASTGenerator();
         ASTNode ast = astgen.translate(tree);
         Interpreter interp = new Interpreter();
