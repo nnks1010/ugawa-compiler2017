8b98af1a9248f7f2db210a2942d72eb8f09b18a5
nnks <ntomoya1010@gmail.com>
2017-12-02 14:33:03 +0900

    exercises 7

--- TinyPiS/src/ASTGenerator.java
+++ TinyPiS/src/ASTGenerator.java
@@ -10,12 +10,14 @@ import parser.TinyPiSParser.OrExprContext;
 import parser.TinyPiSParser.UnExprContext;
 import parser.TinyPiSParser.AddExprContext;
 import parser.TinyPiSParser.ExprContext;
+import parser.TinyPiSParser.IfStmtContext;
 import parser.TinyPiSParser.LiteralExprContext;
 import parser.TinyPiSParser.MulExprContext;
 import parser.TinyPiSParser.ParenExprContext;
 import parser.TinyPiSParser.ProgContext;
 import parser.TinyPiSParser.StmtContext;
 import parser.TinyPiSParser.VarExprContext;
+import parser.TinyPiSParser.WhileStmtContext;
 
 public class ASTGenerator {	
 	ASTNode translate(ParseTree ctxx) {
@@ -39,6 +41,17 @@ public class ASTGenerator {
 			String var = ctx.IDENTIFIER().getText();
 			ASTNode expr = translate(ctx.expr());
 			return new ASTAssignStmtNode(var, expr);
+		} else if (ctxx instanceof IfStmtContext) {
+			IfStmtContext ctx = (IfStmtContext) ctxx;
+			ASTNode cond = translate(ctx.expr());
+			ASTNode thenClause = translate(ctx.stmt(0));
+			ASTNode elseClause = translate(ctx.stmt(1));
+			return new ASTIfStmtNode(cond, thenClause, elseClause);
+		} else if (ctxx instanceof WhileStmtContext) {
+			WhileStmtContext ctx = (WhileStmtContext) ctxx;
+			ASTNode cond = translate(ctx.expr());
+			ASTNode stmt = translate(ctx.stmt());
+			return new ASTWhileStmtNode(cond, stmt);
 		} else if (ctxx instanceof ExprContext) {
 			ExprContext ctx = (ExprContext) ctxx;
 			return translate(ctx.orExpr());
